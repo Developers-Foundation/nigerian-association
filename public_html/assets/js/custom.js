4044a2c614fe20bea1e93f15fe6eb7f84d8d86bf
Nobody Random <iamnobodyrandom@yahoo.com>
2016-06-15 01:12:33 -0400

    Update Gmap

--- public_html/assets/js/custom.js
+++ public_html/assets/js/custom.js
@@ -22,10 +22,38 @@ $('.carousel.three .item').each(function () {
  /* ----------------------------------------------------------- */
 
 
-function loadedGmap () {
+function loadedGmap() {
     if ($('body').hasClass("index-page")) {
         // create a LatLng object containing the coordinate for the center of the map
         var latlng = new google.maps.LatLng(-33.86455, 151.209);
+
+        var styleArr = [{
+            "featureType": "landscape",
+            "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]
+        }, {
+            "featureType": "poi",
+            "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]
+        }, {
+            "featureType": "road.highway",
+            "stylers": [{"saturation": -100}, {"visibility": "simplified"}]
+        }, {
+            "featureType": "road.arterial",
+            "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]
+        }, {
+            "featureType": "road.local",
+            "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]
+        }, {
+            "featureType": "transit",
+            "stylers": [{"saturation": -100}, {"visibility": "simplified"}]
+        }, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {
+            "featureType": "water",
+            "elementType": "labels",
+            "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]
+        }, {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]
+        }];
 
         // prepare the map properties
         var options = {
@@ -35,6 +63,7 @@ function loadedGmap () {
             navigationControl: true,
             mapTypeControl: false,
             scrollwheel: false,
+            styles: styleArr,
             disableDoubleClickZoom: true
         };
 
